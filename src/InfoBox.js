663446dfd78630d9e6a4cb7fc9a3b741f59e3b2a
Facucode <34733879+Facucode@users.noreply.github.com>
2021-05-23 10:42:17 -0300

    Covid and vaccines info and graph

---
+++ src/InfoBox.js
@@ -0,0 +1,24 @@
+import { Card, CardContent, Typography } from '@material-ui/core'
+import React from 'react'
+
+function InfoBox({title, cases, total,}) {
+    return (
+        <div>
+            <Card>
+                <CardContent>
+                    <Typography className="infoBox__title" color="textSecondary">
+                        {title}
+                    </Typography>
+
+                    <h2 className="infoBox__cases">{cases}</h2>
+
+                    <Typography className="infoBox__total" color ="textSecondary">
+                        {total} Total
+                    </Typography>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default InfoBox
---
+++ src/LineGraph.js
@@ -0,0 +1,110 @@
+import React, {useEffect, useState} from 'react'
+import {Line} from "react-chartjs-2"
+import numeral from "numeral"
+
+const options={
+    legend:{
+        display:false
+    },
+    elements:{
+        point:{
+            radius:0
+        }
+    },
+    maintainAspectRatio: false,
+    tooltips:{
+        mode:"index",
+        instersect: false,
+        callbacks: {
+            label: function (tooltipItem, data) {
+                return numeral(tooltipItem.value).format("+0,0")
+            }
+        }
+    },
+    scales:{
+        xAxes:[
+            {
+                type:"time",
+                time: {
+                    format: "MM/DD/YY",
+                    tooltipFormat:"ll"
+                }
+            }
+        ],
+        yAxes:[
+            {
+                gridLines:{
+                    display:false
+                },
+                ticks:{
+                    callback: function(value, index, values) {
+                        return numeral(value).format("0a")
+                    }
+
+                }
+            }
+
+        ]
+    }
+}
+
+function LineGraph() {
+    const [data, setData] = useState({})
+    
+    const buildChartData = (data, casesType='cases') =>{
+        const chartData = [];
+        let lastDataPoint;
+    
+        //data[casesType].cases.forEach(date=>{
+        for(let date in data.cases){  
+        if(lastDataPoint){
+                let newDataPoint = {
+                    x: date,
+                    y: data[casesType][date] - lastDataPoint
+                }
+                chartData.push(newDataPoint)
+            }
+            lastDataPoint=data[casesType][date];
+    
+        }
+    
+    return chartData
+    }
+useEffect(() => {
+    const fetchData = async ()=>{
+    await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+    .then(response => response.json())
+    .then(data =>{
+        console.log(data)
+        const chartData = buildChartData(data, 'cases')
+        setData(chartData)
+    })
+
+}
+fetchData()
+},[])
+
+
+
+return(
+        <div>
+            {data?.length>0 &&( <Line 
+              options={options}
+              data={{
+                  datasets: 
+                      [{
+                          backgroundColor:"rgba(204, 16, 52, 0.5)",
+                          borderColor:"#CC1034",
+                          data:data
+                        }]
+                  
+              }}
+              
+            />
+            )}
+           
+        </div>
+    )
+}
+
+export default LineGraph
